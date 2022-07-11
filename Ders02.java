7c43ed92c29eab30d9fa76ea0e0f7666de0226bb
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-11 19:29:41 +0300

    scanner uygulaması yapıldı.

---
+++ Ders02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ders02 {
+    public static void main(String[] args) {    
+        Scanner input =new Scanner(System.in);
+        int a,b;
+
+        System.out.println("A sayısını giriniz");
+        a=input.nextInt();
+        System.out.println("B sayısını giriniz");
+        b=input.nextInt();
+        System.out.println("A saysı :"+a);
+        System.out.println("B sayısı:"+b);
+        5
+    }
+}
--- ders1.java
+++ ders1.java
@@ -1,6 +1,6 @@
 public class ders1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world \f");
         
     }
 }
