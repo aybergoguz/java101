db6d738d60be49126d38bcd73cb07cc15be4f26a
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-12 15:16:47 +0300

    Vücut Kitle Endeksi Hesaplama

---
+++ Odev6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Odev6{
+
+    public void main(String[] args) {
+
+        double boy,kilo,vucutKitleEndeksi;
+
+        Scanner girilenDeger=new Scanner(System.in);
+
+        System.out.println("Lütfen Boyunuzu Giriniz : ");
+        boy=girilenDeger.nextDouble();
+        System.out.println("Lütfen Kilonuzu Giriniz : ");
+        kilo=girilenDeger.nextDouble();
+
+        vucutKitleEndeksi=kilo/(boy*boy);
+
+        System.out.println("Vücut Kitle Endeksiniz : "+vucutKitleEndeksi);
+
+        
+    }
+}
