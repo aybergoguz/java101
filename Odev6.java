da63a964f2c03e5ca6f67514b3da97ffd43aba76
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-12 15:19:57 +0300

    Vücut Kitle Endeksi Hesaplama düzeltid

--- Odev6.java
+++ Odev6.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Odev6{
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         double boy,kilo,vucutKitleEndeksi;
 
