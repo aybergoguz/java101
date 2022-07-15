511bc52a6fc1a34e432f8f6a8e7b45cb533eb682
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-07-15 13:28:31 +0300

    girilen sayiya kadar 4 ve 5 in katlari

---
+++ Odev17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Odev17{
+
+    public static void main(String[] args) {
+
+        int n;
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Sayi Giriniz : ");
+        n=scanner.nextInt();
+        for(int i=0;i<=n;i++){
+            if(i % 20==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
